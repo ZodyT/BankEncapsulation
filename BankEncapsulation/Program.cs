9b96a9da2be0d7a964b6271af38872a345ecfb9a
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 10:46:33 -0600

    Finished

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -8,20 +8,20 @@ namespace BankEncapsulation
         {
            BankAccount Account1 = new BankAccount();
 
-            Console.WriteLine($"How much would you like to deposit into your account?");
+            Console.WriteLine("How much would you like to deposit into your account?");
             double depositAmount = Convert.ToDouble(Console.ReadLine());
 
             Account1.Deposit(depositAmount);
             double userBalance = Account1.GetBalance();
 
             Console.WriteLine($"Your balance is: {userBalance, 0:c}");
+            Console.WriteLine();
 
-        }
-
-                
-                
+            PersonalInformation Info = new PersonalInformation();
             
-
-        
+            Console.WriteLine("What is your phone number?");
+            Info.NumberFormatted = (Console.ReadLine());
+            Console.WriteLine($"Your phone number is {Info.NumberFormatted}");
+        }              
     }
 }
