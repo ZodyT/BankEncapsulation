35d1abe71edd80732006802aa936fb9f92b224e3
ZodyT <Zoraida1988@yahoo.com>
2020-11-24 18:17:54 -0600

    Finished

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Depositing {amount,0:c} into your account");
+            _balance = amount;
+        }
+        public double GetBalance()
+        {
+           return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           BankAccount Account1 = new BankAccount();
+
+            Console.WriteLine($"How much would you like to deposit into your account?");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+
+            Account1.Deposit(depositAmount);
+            double userBalance = Account1.GetBalance();
+
+            Console.WriteLine($"Your balance is: {userBalance, 0:c}");
+
+        }
+
+                
+                
+            
+
+        
+    }
+}
